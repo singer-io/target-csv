7e974883ab9070c871cdb0126eff5dda1da24f3f
Christopher Merrick <chris@stitchdata.com>
2017-08-10 08:38:15 -0400

    release 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-csv',
-      version='0.2.1',
+      version='0.2.2',
       description='Singer.io target for writing CSV files',
       author='Stitch',
       url='https://singer.io',
