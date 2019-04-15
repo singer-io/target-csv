a98d4633a97e3b7183a99a4f790722535b97c238
Tim Visher <tim@stitchdata.com>
2019-04-15 16:10:10 +0000

    v0.2.4 → v0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='target-csv',
-      version='0.2.4',
+      version='0.3.0',
       description='Singer.io target for writing CSV files',
       author='Stitch',
       url='https://singer.io',
