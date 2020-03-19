4852275f6089454e524b081c1ffef6cfd174bc1b
Alexander Bij <AlexanderBij@godatadriven.com>
2020-03-19 21:42:35 +0100

    rename res to single char x

--- target_csv_test.py
+++ target_csv_test.py
@@ -2,29 +2,29 @@ import target_csv
 
 
 def test_flatten_empty():
-    res = target_csv.flatten({})
-    assert res == {}
+    x = target_csv.flatten({})
+    assert x == {}
 
 
 def test_flatten_simple():
-    res = target_csv.flatten({"f1": 1})
-    assert res == {"f1": 1}
+    x = target_csv.flatten({"f1": 1})
+    assert x == {"f1": 1}
 
 
 def test_flatten_list_simple():
-    res = target_csv.flatten({"f1": ["1", "2", 3]})
-    assert res == {"f1": '["1", "2", 3]'}
+    x = target_csv.flatten({"f1": ["1", "2", 3]})
+    assert x == {"f1": '["1", "2", 3]'}
 
 
 def test_flatten_list_complex():
-    res = target_csv.flatten({"f1": [{"n": 1}, {"n": 2}]})
-    assert res == {"f1": '[{"n": 1}, {"n": 2}]'}
+    x = target_csv.flatten({"f1": [{"n": 1}, {"n": 2}]})
+    assert x == {"f1": '[{"n": 1}, {"n": 2}]'}
 
 
 def test_flatten_list_complex_non_ascii():
-    res = target_csv.flatten(
+    x = target_csv.flatten(
         {"f1": [
             {"japanese": "私の問題を",
              "russian": "Удовлетворены"}]
         })
-    assert res == {"f1": '[{"japanese": "私の問題を", "russian": "Удовлетворены"}]'}
+    assert x == {"f1": '[{"japanese": "私の問題を", "russian": "Удовлетворены"}]'}
